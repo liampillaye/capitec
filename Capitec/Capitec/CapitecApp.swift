fa67e9f1da93cf6837667e59c3dc8e1765df417f
Liam Pillaye <liampillaye@Liams-MacBook-Air.local>
2025-09-23 18:51:51 +0200

    Added new app template scafolding to the repo

---
+++ Capitec/Capitec/CapitecApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapitecApp.swift
+//  Capitec
+//
+//  Created by Liam Pillaye.
+//
+
+import SwiftUI
+
+@main
+struct CapitecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Capitec/CapitecTests/CapitecTests.swift
@@ -0,0 +1,17 @@
+//
+//  CapitecTests.swift
+//  CapitecTests
+//
+//  Created by Liam Pillaye on 2025/09/23.
+//
+
+import Testing
+@testable import Capitec
+
+struct CapitecTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
